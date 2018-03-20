9ce2d911b9c3eb0d3761334af54184f01699e121
Hirak <hiraksamani204@gmail.com>
2018-03-20 22:44:38 +0530

    Fix bottombar issue

--- app/src/main/java/wordtextcounter/details/main/feature/main/MainActivity.kt
+++ app/src/main/java/wordtextcounter/details/main/feature/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package wordtextcounter.details.main.feature.main
 
 import android.os.Bundle
+import android.view.ViewGroup
+import com.orhanobut.logger.Logger
 import com.roughike.bottombar.OnTabSelectListener
 import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_main.bottombar
@@ -10,47 +12,61 @@ import wordtextcounter.details.main.feature.base.BaseActivity
 import wordtextcounter.details.main.feature.input.InputFragment
 import wordtextcounter.details.main.feature.notes.NotesFragment
 import wordtextcounter.details.main.util.RxBus
+import wordtextcounter.details.main.util.dpToPx
 
 class MainActivity : BaseActivity(), OnTabSelectListener {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_main)
-    setSupportActionBar(toolbar)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-    RxBus.instance.subscribe(ToolbarTitle::class.java,
-        Consumer {
-          toolbar.setTitle(it.title)
-        })
+        RxBus.instance.subscribe(ToolbarTitle::class.java,
+                Consumer {
+                    Logger.d("Subscribe $it")
+                    toolbar.setTitle(it.title)
+                })
 
-    bottombar.setOnTabSelectListener(this)
+        val activityRootView = findViewById<ViewGroup>(android.R.id.content).getChildAt(0)
+        activityRootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val heightDiff =
+                    activityRootView.rootView.height - activityRootView.height
+            if (heightDiff > dpToPx(this@MainActivity,
+                            200F)) { // if more than 200 dp, it's probably a keyboard...
+                bottombar.shySettings.hideBar()
+            } else {
+                bottombar.shySettings.showBar()
+            }
+        }
 
-    supportFragmentManager.addOnBackStackChangedListener {
-      val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
-      bottombar.removeOnTabSelectListener()
-      if (currentFragment is InputFragment) {
-        bottombar.selectTabWithId(R.id.tab_input)
-      } else if (currentFragment is NotesFragment) {
-        bottombar.selectTabWithId(R.id.tab_notes)
-      }
-      bottombar.setOnTabSelectListener(this, false)
+        bottombar.setOnTabSelectListener(this)
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
+            bottombar.removeOnTabSelectListener()
+            if (currentFragment is InputFragment) {
+                bottombar.selectTabWithId(R.id.tab_input)
+            } else if (currentFragment is NotesFragment) {
+                bottombar.selectTabWithId(R.id.tab_notes)
+            }
+            bottombar.setOnTabSelectListener(this, false)
+        }
+
     }
 
-  }
-
-  override fun onBackPressed() {
-    if (supportFragmentManager.backStackEntryCount == 1) {
-      finish()
-    } else {
-      super.onBackPressed()
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 1) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
     }
-  }
 
-  override fun onTabSelected(tabId: Int) {
-    when (tabId) {
-      R.id.tab_input -> replaceFragment(InputFragment.newInstance())
-      R.id.tab_notes -> replaceFragment(NotesFragment.newInstance())
+    override fun onTabSelected(tabId: Int) {
+        when (tabId) {
+            R.id.tab_input -> replaceFragment(InputFragment.newInstance())
+            R.id.tab_notes -> replaceFragment(NotesFragment.newInstance())
+        }
     }
-  }
 }
