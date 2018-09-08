3cabfc54b0cc93aeb6e5e477a875e8bf91fc5c69
Hirak <hirak@myglamm.com>
2018-09-09 01:58:14 +0530

    Move expandable code to base abstract adapter

---
+++ app/src/main/java/wordtextcounter/details/main/feature/input/AbstractExpandableAdapter.kt
@@ -0,0 +1,97 @@
+package wordtextcounter.details.main.feature.input
+
+import android.support.v7.widget.RecyclerView
+import android.util.SparseArray
+import android.util.SparseIntArray
+import android.view.ViewGroup
+import androidx.core.util.set
+
+abstract class AbstractExpandableAdapter<GVH : RecyclerView.ViewHolder, CVH : RecyclerView.ViewHolder>
+  : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+  private val headerExpandTracker = SparseIntArray()
+  private val viewTypes: SparseArray<ViewType> = SparseArray()
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    return if (viewType == TYPE_HEADER) {
+      val holder = createGroupViewHolder(parent, viewType)
+      holder.itemView.setOnClickListener {
+        if (holder.adapterPosition != RecyclerView.NO_POSITION) {
+          onHeaderClicked(holder.adapterPosition)
+        }
+      }
+      holder
+    } else {
+      createChildViewHolder(parent, viewType)
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    val viewType = getItemViewType(position)
+    val index = viewTypes[position].index
+    val groupIndex = viewTypes[position].groupIndex
+    if (viewType == TYPE_HEADER) {
+      bindGroupViewHolder(holder as GVH, index)
+    } else {
+      bindChildViewHolder(holder as CVH, groupIndex, index)
+    }
+  }
+
+  override fun getItemViewType(position: Int): Int {
+    return viewTypes[position].type
+  }
+
+  override fun getItemCount(): Int {
+    var totalCount = 0
+    viewTypes.clear()
+    var collapsedCount = 0
+    for (i in 0 until groupCount()) {
+      viewTypes.put(totalCount, ViewType(i, TYPE_HEADER))
+      totalCount++
+      val childCount = childCount(i)
+      if (headerExpandTracker[i] != 0) {
+        //Expanded
+        for (j in 0 until childCount) {
+          viewTypes.put(totalCount, ViewType(j, TYPE_CHILD, i))
+          totalCount++
+        }
+      } else {
+        //collapsed
+        collapsedCount += childCount
+      }
+    }
+    return totalCount
+  }
+
+  private fun onHeaderClicked(position: Int) {
+    val index = viewTypes[position].index
+    val childCount = childCount(index)
+    if (headerExpandTracker[index] == 0) {
+      //collapsed, expand now
+      headerExpandTracker[index] = 1
+      notifyItemRangeInserted(position + 1, childCount)
+    } else {
+      //expanded, collapse now
+      headerExpandTracker[index] = 0
+      notifyItemRangeRemoved(position + 1, childCount)
+    }
+  }
+
+  abstract fun createGroupViewHolder(parent: ViewGroup, viewType: Int): GVH
+
+  abstract fun groupCount(): Int
+
+  abstract fun childCount(groupPosition: Int): Int
+
+  abstract fun createChildViewHolder(parent: ViewGroup, viewType: Int): CVH
+
+  abstract fun bindGroupViewHolder(viewHolder: GVH, groupPosition: Int)
+
+  abstract fun bindChildViewHolder(viewHolder: CVH, groupPosition: Int, childPosition: Int)
+}
+
+data class ViewType(val index: Int, val type: Int, val groupIndex: Int = -1)
+
+const val TYPE_HEADER = 0
+const val TYPE_CHILD = 1
--- app/src/main/java/wordtextcounter/details/main/feature/input/ExtraStatsAdapter.kt
+++ app/src/main/java/wordtextcounter/details/main/feature/input/ExtraStatsAdapter.kt
@@ -1,94 +1,55 @@
 package wordtextcounter.details.main.feature.input
 
 import android.support.v7.widget.RecyclerView
-import android.util.SparseArray
-import android.util.SparseIntArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.util.set
 import kotlinx.android.synthetic.main.item_extra_stat_child.view.*
 import kotlinx.android.synthetic.main.item_extra_stat_header.view.*
 import wordtextcounter.details.main.R
 
-class ExtraStatsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ExtraStatsAdapter : AbstractExpandableAdapter<ExtraStatsAdapter.HeaderViewHolder, ExtraStatsAdapter.ChildViewHolder>() {
 
   private var statGroups: List<ExtraStatGroup> = mutableListOf()
-  private val headerExpandTracker = SparseIntArray()
-  private val viewTypes: SparseArray<ViewType> = SparseArray()
 
   fun setStats(statGroups: List<ExtraStatGroup>) {
     this.statGroups = statGroups
-    for (i in 0 until statGroups.size) {
-      headerExpandTracker[i] = 1
-    }
   }
 
-  override fun getItemViewType(position: Int): Int {
-    return viewTypes[position].type
+  override fun createGroupViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
+    return HeaderViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_extra_stat_header, parent,
+            false))
   }
 
-  override fun getItemCount(): Int {
-    var totalCount = 0
-    viewTypes.clear()
+  override fun groupCount() = statGroups.size
 
-    var collapsedCount = 0
-    for (i in 0 until statGroups.size) {
-      viewTypes.put(totalCount, ViewType(i, TYPE_HEADER))
-      totalCount++
-      val statGroup = statGroups[i]
-      val childCount = statGroup.stats.size
-      if (headerExpandTracker[i] != 0) {
-        //Expanded
-        for (j in 0 until childCount) {
-          viewTypes.put(totalCount, ViewType(j, TYPE_CHILD, i))
-          totalCount++
-        }
-      } else {
-        //collapsed
-        collapsedCount += childCount
-      }
-    }
-    return totalCount
+  override fun childCount(groupPosition: Int) = statGroups[groupPosition].stats.size
+
+  override fun createChildViewHolder(parent: ViewGroup, viewType: Int): ChildViewHolder {
+    return ChildViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_extra_stat_child, parent,
+            false))
   }
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-    return if (viewType == TYPE_HEADER) {
-      HeaderViewHolder(
-          LayoutInflater.from(parent.context).inflate(R.layout.item_extra_stat_header, parent,
-              false))
-    } else {
-      ChildViewHolder(
-          LayoutInflater.from(parent.context).inflate(R.layout.item_extra_stat_child, parent,
-              false))
-    }
+  override fun bindGroupViewHolder(viewHolder: HeaderViewHolder, groupPosition: Int) {
+    viewHolder.bindTo(statGroups[groupPosition])
   }
 
-  override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-    val viewType = getItemViewType(position)
-    val index = viewTypes[position].index
-    val groupIndex = viewTypes[position].groupIndex
-    if (viewType == TYPE_HEADER) {
-      (holder as HeaderViewHolder).bindTo(statGroups[index])
-    } else {
-      (holder as ChildViewHolder).bindTo(statGroups[groupIndex].stats[index])
-    }
+  override fun bindChildViewHolder(viewHolder: ChildViewHolder, groupPosition: Int,
+      childPosition: Int) {
+    viewHolder.bindTo(statGroups[groupPosition].stats[childPosition])
   }
 
   inner class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val tvExtraStatGroupName = view.tvExtraStatGroupName
 
-    init {
-      view.setOnClickListener {
-        onHeaderClicked(adapterPosition)
-      }
-    }
-
     fun bindTo(statGroup: ExtraStatGroup) {
       tvExtraStatGroupName.text = statGroup.groupName
     }
   }
+
 
   inner class ChildViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -100,25 +61,4 @@ class ExtraStatsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
       tvExtraStatValue.text = extraStat.value
     }
   }
-
-  fun onHeaderClicked(position: Int) {
-    val index = viewTypes[position].index
-    val clickedStatGroup = statGroups[index]
-    val childCount = clickedStatGroup.stats.size
-    if (headerExpandTracker[index] == 0) {
-      //collapsed, expand now
-      headerExpandTracker[index] = 1
-      notifyItemRangeInserted(position + 1, childCount)
-    } else {
-      //expanded, collapse now
-      headerExpandTracker[index] = 0
-      notifyItemRangeRemoved(position + 1, childCount)
-    }
-  }
-
 }
-
-data class ViewType(val index: Int, val type: Int, val groupIndex: Int = -1)
-
-const val TYPE_HEADER = 0
-const val TYPE_CHILD = 1
