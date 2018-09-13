a6cd9cb9cad6d2ae1f65c3edb6f5e7fff7afa98f
Ankit <apsaliya@gmail.com>
2018-09-13 21:47:19 +0530

    Insert DraftDao.

---
+++ app/src/main/java/wordtextcounter/details/main/store/daos/DraftDao.kt
@@ -0,0 +1,16 @@
+package wordtextcounter.details.main.store.daos
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import wordtextcounter.details.main.store.entities.Draft
+import wordtextcounter.details.main.store.entities.DraftHistory
+
+@Dao
+interface DraftDao {
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun saveDraft(draft: Draft)
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun saveDraftHistory(draftHistory: DraftHistory)
+}
