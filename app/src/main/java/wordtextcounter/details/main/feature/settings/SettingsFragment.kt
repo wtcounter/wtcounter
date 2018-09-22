fca33ccda5fc3990b3bb94d00ad28437f76dc99d
Hirak <hiraksamani204@gmail.com>
2018-09-23 04:52:28 +0530

    Show current value in dialog summary, update summary when value changed

--- app/src/main/java/wordtextcounter/details/main/feature/settings/SettingsFragment.kt
+++ app/src/main/java/wordtextcounter/details/main/feature/settings/SettingsFragment.kt
@@ -3,12 +3,39 @@ package wordtextcounter.details.main.feature.settings
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 import wordtextcounter.details.main.R
+import wordtextcounter.details.main.util.Constants
 
-class SettingsFragment: PreferenceFragmentCompat()  {
-
+class SettingsFragment : PreferenceFragmentCompat() {
 
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
     addPreferencesFromResource(R.xml.settings)
+
+    val readingTimePref = preferenceManager.findPreference(
+        Constants.PREF_READING_TIME_WORDS_PER_MINUTE)
+    readingTimePref.summary = preferenceManager.sharedPreferences.getString(
+        Constants.PREF_READING_TIME_WORDS_PER_MINUTE, "")
+    readingTimePref.setOnPreferenceChangeListener { _, newValue ->
+      readingTimePref.summary = newValue.toString()
+      true
+    }
+
+    val speakingTimePref = preferenceManager.findPreference(
+        Constants.PREF_SPEAKING_TIME_WORDS_PER_MINUTE)
+    speakingTimePref.summary = preferenceManager.sharedPreferences.getString(
+        Constants.PREF_SPEAKING_TIME_WORDS_PER_MINUTE, "")
+    speakingTimePref.setOnPreferenceChangeListener { _, newValue ->
+      speakingTimePref.summary = newValue.toString()
+      true
+    }
+
+    val writingTimePref = preferenceManager.findPreference(
+        Constants.PREF_WRITING_TIME_LETTERS_PER_SECOND)
+    writingTimePref.summary = preferenceManager.sharedPreferences.getString(
+        Constants.PREF_WRITING_TIME_LETTERS_PER_SECOND, "")
+    writingTimePref.setOnPreferenceChangeListener { _, newValue ->
+      writingTimePref.summary = newValue.toString()
+      true
+    }
   }
 
   companion object {
--- app/src/main/java/wordtextcounter/details/main/util/Constants.kt
+++ app/src/main/java/wordtextcounter/details/main/util/Constants.kt
@@ -3,8 +3,12 @@ package wordtextcounter.details.main.util
 
 object Constants {
   const val PREF_NAME = "wtc.preferences"
-  
+
   const val PREF_DB_STORED = "wtc.preferences.stored_dbversion"
   const val PREF_ANALYTICS_ENABLED = "wtc.preferences.analytics_enabled"
   const val PREF_ANALYTICS_CONSENT = "wtc.preferences.analytics_consent"
+
+  const val PREF_READING_TIME_WORDS_PER_MINUTE = "reading_wpm"
+  const val PREF_SPEAKING_TIME_WORDS_PER_MINUTE = "speaking_wpm"
+  const val PREF_WRITING_TIME_LETTERS_PER_SECOND = "writing_lps"
 }
